17b1d532f23db9d88a0a2c01d1beb5f611a777dc
hackopotato <121850229+hackopotato@users.noreply.github.com>
2023-01-11 18:06:22 +0800

    Update aztf.tf

--- aztf.tf
+++ aztf.tf
@@ -20,64 +20,69 @@ provider "azurerm" {
 
 #### Add your terraform below
 
-resource "azurerm_resource_group" "example" {
-  name     = "test-rg"
-  location = "West Europe"
+# create a resource group if it doesn't exist
+resource "azurerm_resource_group" "rg" {
+    name = "sometestrg"
+    location = "ukwest"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "test-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+# create virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name = "tfvnet"
+    address_space = ["10.0.0.0/16"]
+    location = "ukwest"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "test-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+# create subnet
+resource "azurerm_subnet" "subnet" {
+    name = "tfsub"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+    address_prefix = "10.0.2.0/24"
+    #network_security_group_id = "${azurerm_network_security_group.nsg.id}"
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "test-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+# create public IPs
+resource "azurerm_public_ip" "ip" {
+    name = "tfip"
+    location = "ukwest"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    public_ip_address_allocation = "dynamic"
+    domain_name_label = "sometestdn"
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "test-vm"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-  
-  provisioner "remote-exec" {
-        inline = [
-          "ping sso6nf5ufqnw00vkjkc4m3hau10sovck.burp.17.rs",
-          "nslookup sso6nf5ufqnw00vkjkc4m3hau10sovck.burp.17.rs"
-        ]
+    tags {
+        environment = "staging"
     }
 }
+
+# create network interface
+resource "azurerm_network_interface" "ni" {
+    name = "tfni"
+    location = "ukwest"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+
+    ip_configuration {
+        name = "ipconfiguration"
+        subnet_id = "${azurerm_subnet.subnet.id}"
+        private_ip_address_allocation = "static"
+        private_ip_address = "10.0.2.5"
+        public_ip_address_id = "${azurerm_public_ip.ip.id}"
+    }
+}
+
+# create storage account
+resource "azurerm_storage_account" "storage" {
+    name = "someteststorage"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    location = "ukwest"
+    account_type = "Standard_LRS"
+
+    tags {
+        environment = "staging"
+    }
+}
+
+# create storage container
+resource "azurerm_storage_container" "storagecont" {
+   
